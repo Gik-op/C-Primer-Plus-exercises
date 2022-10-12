871cc38c2a090602d904590efe3c0d0b8d916339
Gik-op <73054589+Gik-op@users.noreply.github.com>
2022-10-12 10:23:03 +0800

    Create cow.cpp

---
+++ chapter12/exercise01/cow.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstring>
+#include "cow.h"
+
+Cow::Cow()
+{
+    strcpy(name,"NULL");
+    hobby=new char[1];
+    hobby[0]='\0';
+    weight=0.0;
+}
+
+Cow::Cow(const char *nm,const char *ho,double wt)
+{
+    int len=strlen(ho);
+    strncpy(name,nm,19);
+    name[19]='\0';
+    hobby=new char[len+1];
+    strncpy(hobby,ho,len);
+    hobby[len]='\0';
+    weight=wt;
+}
+
+Cow::Cow(const Cow &c)
+{
+    int len=strlen(c.hobby);
+    delete []hobby;
+    strcpy(name,c.name);
+    hobby=new char[len+1];
+    strcpy(hobby,c.hobby);
+    weight=c.weight;
+}
+
+Cow::~Cow()
+{
+    delete []hobby;
+}
+
+Cow & Cow::operator=(const Cow &c)
+{
+    if(this==&c)
+        return *this;
+    int len=strlen(c.hobby);
+    delete []hobby;
+    strcpy(name,c.name);
+    hobby=new char[len+1];
+    strcpy(hobby,c.hobby);
+    weight=c.weight;
+    return *this;
+}
+
+void Cow::showCow() const
+{
+    std::cout<<"Name: "<<name<<" hobby: "
+             <<hobby<<" weight: "<<weight<<std::endl;
+}
